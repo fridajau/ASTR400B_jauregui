1deddddfc809db07a7c4bc10a6a223821f5efc7b
fridajau <fjauregui@email.arizona.edu>
2018-01-22 18:21:30 -0700

    finished hw2 code

--- hw2/ParticlesProperties.py
+++ hw2/ParticlesProperties.py
@@ -1,25 +1,51 @@
 """
 Frida Jauregui
+Started Jan 16
 ASTR400b HW2 Due Jan 23
 """
 
 import numpy as np
 import astropy.units as u
-from ReadFile import Read 
 
-def ParticleInfo(distance,velocity,mass):
-    np.around(vaule,3)
-    mass = data['m']*1.0e10*u.M_solarmass          #assigning mass from data in ReadFile
-    index = np.where(data['m'][:100])              #the mass of the 100th particle
-    mnew = data['m'][index]                        #mass of the particle with the given property
-    distance = data['x','y','z']*u.kpc
-    iindex = np.where(data['x','y','z'][:100])     #the distance of the 100th particle
-    dnew = data['x','y','z'][iindex]
-    velocity = data['vx','vy','vz']*u.km/s
-    iiindex = np.where(data['vx','vy','vz'][:100]) #the velocity of the 100th particle
-    vnew = data['vx','vy','vz'][iiindex]
+data = np.genfromtxt("MW_000.txt",dtype=None,names=True,
+                     skip_header=3)
 
-    return dnew,vnew,mnew
+#mass = float(value)*1.0e10*u.M_solarmass      #assigning mass from data in the text
+Mindex = np.where(data['m'][:100])             #the mass of the 100th particle
+mnew = data['m'][Mindex]                       #mass of the particle with the given property
+
+#distance = float(value)*1.0*u.kpc             #assigning pos from data in the text
+Xindex = np.where(data['x'][:100])             #the x position of the 100th particle
+dxnew = data['x'][Xindex]
+                      
+Yindex = np.where(data['y'][:100])             #the y position of the 100th particle
+dynew = data['y'][Yindex]
+    
+Zindex = np.where(data['z'][:100])             #the z position of the 100th particle
+dznew = data['z'][Zindex]
+    
+#velocity = float(value)*1.0*u.km/s            #assigning vel from data in the text
+Vxindex = np.where(data['vx'][:100])           #the x velocity of the 100th particle
+vxnew = data['vx'][Vxindex]
+        
+Vyindex = np.where(data['vy'][:100])           #the y velocity of the 100th particle
+vynew = data['vy'][Vyindex]
+
+Vzindex = np.where(data['vz'][:100])           #the z velocity of the 100th particle
+vznew = data['vz'][Vzindex]
+
+print"Mass, X Position, Y Position, Z Position, X Velocity, Y Velocity, Z Velocity"
+print(mnew[-1],dxnew[-1],dynew[-1],dznew[-1],vxnew[-1],vynew[-1],vznew[-1])
+
+#---notes
+"""
+np.where(data['dtype'][:100]) gives me 100 dtype data points, I can print the
+last line of the array by array[-1]
+
+keeping this as a function I couldnt print anything back, it just compiled so I just wrote
+this assignment as lines of code
+"""
+
     
     
-    
+
--- hw2/ReadFile.py
+++ hw2/ReadFile.py
@@ -1,31 +1,48 @@
 """
 Frida Jauregui
+Started Jan 16
 ASTR400b HW2 Due Jan 23
 """
-
 import numpy as np
 import astropy.units as u
 
-def Read(MW_000):
-    file = open(MW_000, 'r')         #open text file
-    line1 = file.readline()          #reading first line and storing the time
-    label, value = line1.split()
-    time = float(value)*10.0*u.Myr
 
-    line2 = file.readline()
-    label, value = line2.split()     #reading first line and storing particles
-    particles = float(value)
+filename = "MW_000.txt"                #assign the file
+file = open(filename,'r')              #open the file and read
+line1 = file.readline()                #read line 1
+label, value = line1.split()           
+time = float(value)*10.0*u.Myr         #store line 1 units
 
-    file.close()
+line2 = file.readline()                
+label, value = line2.split()           #assign line 2 a label and value
+particles = float(value)
 
-    data = np.genfromtxt(MW_000, dtype=None, names=True, skip_header=3)
-    #storing the remainder of the MW_000 file to be able to call back
-    return time, particles, data
+print"Time, Particles"
+print(time, particles)
 
-
-
+file.close()                           #close file
     
-    
+#store the rest of the file starting from line 4
+data = np.genfromtxt("MW_000.txt",dtype=None,names=True,
+                     skip_header=3)
+#return time, total particles and data to call back
+print"Data:"
+print(data)
 
-    
-    
+"""
+###----check and extract  data
+
+data = np.genfromtxt("MW_000.txt",dtype=None,names=True,
+                     skip_header=3)
+index = np.where(data['x'][:100])
+xnew = data['x'][index]
+print"X-position:"
+print(xnew[-1])
+"""
+
+###---notes
+"""
+keeping this as a function I couldnt print anything back, it just 
+compiled so I just wrote this assignment as lines of code and printed 
+the time, total number of particles, and the data as lines of code 
+"""
