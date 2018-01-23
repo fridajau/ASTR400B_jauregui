926064e0b8e181064a83a3670eb06dcfbea37a8e
fridajau <fjauregui@email.arizona.edu>
2018-01-23 15:38:00 -0700

    posistion and velocity conversions added

--- hw2/ParticlesProperties.py
+++ hw2/ParticlesProperties.py
@@ -7,36 +7,52 @@ ASTR400b HW2 Due Jan 23
 import numpy as np
 import astropy.units as u
 
+#---Data
 data = np.genfromtxt("MW_000.txt",dtype=None,names=True,
                      skip_header=3)
+
+#---Want Disk particle
+indextype = np.where(data['type']>1)
+newdata = data[:][indextype]
+"""
+#---check
+print"newdata:"
+print(newdata)
+"""
+
 #---Mass
-Mindex = np.where(data['m'][:100])             #the mass of the 100th particle
-mnew = data['m'][Mindex]*1.0e10*u.solMass      #assigning mass from data in the text
+Mindex = np.where(newdata['m'][:100])             #the mass of the 100th particle
+mnew = newdata['m'][Mindex]*1.0e10*u.solMass      #assigning mass from data in the text
 
 #---Position            
-Xindex = np.where(data['x'][:100])             #the x position of the 100th particle
-dxnew = data['x'][Xindex]*1.0*u.kpc            #assigning pos and quantity
+Xindex = np.where(newdata['x'][:100])             #the x position of the 100th particle
+dxnew = newdata['x'][Xindex]*1.0*u.kpc            #assigning pos and quantity
                       
-Yindex = np.where(data['y'][:100])             #the y position of the 100th particle
-dynew = data['y'][Yindex]*1.0*u.kpc
+Yindex = np.where(newdata['y'][:100])             #the y position of the 100th particle
+dynew = newdata['y'][Yindex]*1.0*u.kpc
     
-Zindex = np.where(data['z'][:100])             #the z position of the 100th particle
-dznew = data['z'][Zindex]*1.0*u.kpc
+Zindex = np.where(newdata['z'][:100])             #the z position of the 100th particle
+dznew = newdata['z'][Zindex]*1.0*u.kpc
     
 #---Velocity
-Vxindex = np.where(data['vx'][:100])           #the x velocity of the 100th particle
-vxnew = data['vx'][Vxindex]*1.0*u.kilometer/u.second
+Vxindex = np.where(newdata['vx'][:100])           #the x velocity of the 100th particle
+vxnew = newdata['vx'][Vxindex]*1.0*u.kilometer/u.second
         
-Vyindex = np.where(data['vy'][:100])           #the y velocity of the 100th particle
-vynew = data['vy'][Vyindex]*1.0*u.kilometer/u.second         
+Vyindex = np.where(newdata['vy'][:100])           #the y velocity of the 100th particle
+vynew = newdata['vy'][Vyindex]*1.0*u.kilometer/u.second         
 
-Vzindex = np.where(data['vz'][:100])           #the z velocity of the 100th particle
-vznew = data['vz'][Vzindex]*1.0*u.kilometer/u.second
+Vzindex = np.where(newdata['vz'][:100])           #the z velocity of the 100th particle
+vznew = newdata['vz'][Vzindex]*1.0*u.kilometer/u.second
 
 #---3D distance
 distance = np.sqrt(dxnew[-1]**2 + dynew[-1]**2 + dznew[-1]**2)
 print"3D-distance:"
 print(distance)
+
+#---3D
+velocity = np.sqrt(vxnew[-1]**2 + vynew[-1]**2 + vznew[-1]**2)
+print"3D-velocity:"
+print(velocity)
 
 #---convert distance using astropy
 lyr = u.lyr
