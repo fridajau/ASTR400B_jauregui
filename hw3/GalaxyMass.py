c705e38bb4ab592a09de826dfa8cb794a6d88062
fridajau <fjauregui@email.arizona.edu>
2018-01-25 12:11:35 -0700

    hw3 folder

---
+++ hw3/GalaxyMass.py
@@ -0,0 +1,12 @@
+"""
+Frida Jauregui
+ASTR400b HW3 Due Jan 30th
+Started Jan 25th
+
+compute the mass breakdown of the Local Group (snap# 0) using MW, M31, and M31
+"""
+
+import numpy as np
+import astropy.units as u
+from ReadFile import Read
+
