071c494031ba534d9791f40eb25e56439e768043
fridajau <fjauregui@email.arizona.edu>
2018-01-25 13:47:58 -0700

    hw 3 code only prints halo component

--- hw3/GalaxyMass.py
+++ hw3/GalaxyMass.py
@@ -10,3 +10,31 @@ import numpy as np
 import astropy.units as u
 from ReadFile import Read
 
+"""
+#---notes
+input PType:    Halo(1), Disk(2), Bluge(3) 
+      filename: "MW00,MW31,MW33"
+return total mass
+"""
+def ComponentMass(PType, filename):
+    #---to be read in the file
+    time, total, data = Read(filename)
+
+    #---an array to store Ptype
+    index = np.where(data['type'] == PType)
+
+    #---Mass
+    mnew = data['m'][index]*1e10*u.Msun
+    totalmass = np.around(mnew[index],3)              
+    return totalmass
+
+#---Total mass of MW Halo
+TM_MWH = ComponentMass(1, "MW_000.txt")
+print("the Milky Way mass components")
+print("Total Mass of Halo Component:",np.around(TM_MWH,3))
+#---Total mass of MW Disk
+#TM_MWD = ComponentMass(2, "MW_000.txt")
+#print("")
+#print("Total Mass of Disk Componet:",np.around(TM_MWD),3)
+
+    
