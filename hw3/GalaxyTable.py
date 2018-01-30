ec494e4aa6364487b5e5855fbc3fff6e09ca2dff
fridajau <fjauregui@email.arizona.edu>
2018-01-29 17:40:46 -0700

    table as txt

--- hw3/GalaxyTable.py
+++ hw3/GalaxyTable.py
@@ -25,6 +25,7 @@ Save: as PDF BONUS if table is created by LaTeX
 
 3.QUESTIONS
 """
+tabletxt = 'HW3table.txt'
 
 ###Mass Break Down###
 
@@ -53,10 +54,12 @@ TM_M33H = np.around(ComponentMass(1, "M33_000.txt")/1e12, 3)
 TM_M33D = np.around(ComponentMass(2, "M33_000.txt")/1e12, 3)
 
 
+
+
 #---Total Mass of each Galaxy and Baryon Fraction
 TMMW  = np.around(TM_MWH + TM_MWD + TM_MWB, 3)               
 TMM31 = np.around(TM_M31H + TM_M31D + TM_M31B, 3)           
-TMM33 = np.around(TM_M33H + TM_M33D, 3)                      
+TMM33 = np.around(TM_M33H + TM_M33D, 3)
 TMLG  = np.around(TMMW + TMM31 + TMM33, 3)
 #---Check with print statements
 fmw  = np.around((TM_MWD+TM_MWB)/(TMMW), 4)
@@ -64,20 +67,21 @@ fm31 = np.around((TM_M31D+TM_M31B)/(TMM31), 4)
 fm33 = np.around((TM_M33D)/(TMM33),4)
 flg  = np.around((TM_MWD+TM_MWB+TM_M31D+TM_M31B+TM_M33D)/(TMLG), 4)
 
-u = u.Msun
+u = 1e12*u.Msun
 
 #---Table
-
 t = Table()
-
+#---Assign the columns and add them to the table
 galacol = Column(name='Galaxy Name', data=['Milky Way','M31','M33'])
-#discol  = Column(name='Disk Mass',   data=[TM_MWD,TM_M31D,TM_M33D], unit=u)
-bulcol  = Column(name='Bulge Mass',  data=[TM_MWB,TM_M31B,' '], unit=u)
-#totcol  = Column(name='Total Mass',  data=[TMMW,TMM31,TMM33], unit=u)
+halcol  = Column(name='Halo Mass',   data=[TM_MWH,TM_M31H,'0.187'], unit=u)
+discol  = Column(name='Disk Mass',   data=[TM_MWD,TM_M31D,'0.009'], unit=u)
+bulcol  = Column(name='Bulge Mass',  data=[TM_MWB,TM_M31B,''], unit=u)
+totcol  = Column(name='Total Mass',  data=[TMMW,TMM31,'0.196'], unit=u)
 t.add_column(galacol)
-#t.add_column(discol)
+t.add_column(halcol)
+t.add_column(discol)
 t.add_column(bulcol)
-#t.add_column(totcol)
+t.add_column(totcol)
 
 fabrcol = Column(name='f_bar', data=[fmw,fm31,fm33])
 t.add_column(fabrcol)
@@ -87,11 +91,19 @@ flgcol   =  Column(name='f_bar for Local Group',     data=[flg, '', ''])
 t.add_column(totlgcol)
 t.add_column(flgcol)
 
-t.info
-
+#---Print and save as text with LaTex
 print(t)
+t.write(tabletxt, format='latex', overwrite=True)
 
 """
 #---notes
 I get an ValueError: setting an array element with a sequence in several columns w/out knowing why.
+so far I printed and obtained the vaules for:
+M33H, M33D, and TMM33
+print("halo mass m33")
+print(TM_M33H)
+print("disk mass m33")
+print(TM_M33D)
+print("tot mass m33")
+print(TMM33)
 """
