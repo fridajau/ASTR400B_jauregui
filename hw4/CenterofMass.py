b2d3fa67b3b6f3a48a0fc69601c2cb753483a911
fridajau <fjauregui@email.arizona.edu>
2018-02-05 15:15:14 -0700

    argument error

--- hw4/CenterofMass.py
+++ hw4/CenterofMass.py
@@ -42,11 +42,13 @@ class CenterOfMass:
 #---Output: returns the 3D coords of the COM(position OR velocity)
 
     def COMdefine(self, x,y,z, m):
+        #---position
         pos = (np.sqrt((self.x)**2 + (self.y)**2 + (self.z)**2))*u.kpc
        
         #---velocity
-        #vel = (np.sqrt((self.vx)**2 + (self.vy)**2 + (self.vz)**2))*u.km/u.s
-        
+        vel = (np.sqrt((self.vx)**2 + (self.vy)**2 + (self.vz)**2))*u.km/u.s
+
+        #---total mass
         totmass = np.sum(self.m)*u.Msun*1e10
         
         #---while loop to return 3D position and total mass 
@@ -59,14 +61,14 @@ class CenterOfMass:
 #---Input:  tolerance
 #---Output: returns an array with the x,y,z coords of the COM position
 
-    def COM_P(self, x,y,z, m, delta):
+    def COM_P(self, x,y,z, m):
         totmass = np.sum(self.m)*u.Msun*1e10
         
         #---COM position vector 
         XCOM = (self.x*totmass)/totmass
         YCOM = (self.y*totmass)/totmass
         ZCOM = (self.z*totmass)/totmass
-        
+        #---magnitude of the new position vector
         RCOM = np.linalg.norm(self.COMdefine(self, XCOM,YCOM,ZCOM, m))
         
         #---changing particle position vector 
@@ -77,7 +79,7 @@ class CenterOfMass:
         RNEW = np.linalg.norm(self.COMdefine(self, XNEW,YNEW,ZNEW, m))
 #######################################
         #---maximum 3D separation of the particles 
-        RMAX = 0.5*(np.linalg.norm(self.x, self.y, self.z))
+        RMAX = 0.5*(np.linalg.norm(self.x, self.y, self.z, m))
 
         #---while loop for RCOM - new COM > delta
         delta  = 10
@@ -105,7 +107,7 @@ class CenterOfMass:
     
 #---prints COM position and velocity of MW disk properties
 MWCOM = CenterOfMass("MW_000.txt", 2)
-MW_P  = MWCOM.COM_P(0,0,0,0)
+#MW_P  = MWCOM.COM_P(0,0,0,0)
 #print("MW COM Disk:", MWCOM)
 
 #---prints vaule of disk mass of galaxy and the 3d position vector              
