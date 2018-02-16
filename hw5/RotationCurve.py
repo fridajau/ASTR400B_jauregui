378b96cd91ea8b42564316d3fb9cd572600352a3
fridajau <fjauregui@email.arizona.edu>
2018-02-16 10:20:20 -0700

    updated code with mass profiles and rotation curves

--- hw5/RotationCurve.py
+++ hw5/RotationCurve.py
@@ -12,6 +12,8 @@ from ReadFile import Read
 from CenterofMass import CenterOfMass
 import matplotlib.pyplot as plt
 from astropy.constants import G
+import matplotlib
+
 
 
 #---Class
@@ -50,29 +52,25 @@ class MassProfile:
         """
     def MassEnclosed(self, ptype, R):
         #---determine COM position by creating a com object and calling COM_P
-        COM    = CenterOfMass(self.filename, ptype)
-        COMpos = COM.COM_P(1.0)
-        Mass   = np.zeros(np.size(R))
+        COM    = CenterOfMass(self.filename, 2)
+        GCOM   = COM.COM_P(1.0, 2.0)
+        Menc   = np.zeros(np.size(R))
         index  = np.where(self.data['type'] == ptype)
         #---find the COM of the positions
-        m2 = self.m[index]
+        mG = self.m[index]
         
-        x2 = self.x[index] - float(COMpos[0]/u.kpc)
-        y2 = self.y[index] - float(COMpos[1]/u.kpc)
-        z2 = self.z[index] - float(COMpos[2]/u.kpc)
+        xG = self.x[index] - float(GCOM[0]/u.kpc)
+        yG = self.y[index] - float(GCOM[1]/u.kpc)
+        zG = self.z[index] - float(GCOM[2]/u.kpc)
         
-        R2 = np.sqrt(x2**2 + y2**2 + z2**2)
+        rG = np.sqrt(xG**2 + yG**2 + zG**2)
         #---loop over the radius to define particles that are enclosed within the radius given
         for i in range(np.size(R)):
             #---store masses within radii
-            index2  = np.where(R2 < R[i])
-            mnew    = m2[index2]
-            #---mass enclosed
-            massenc = np.sum(mnew)
-            #---returns an array
-            Mass[i] = (massenc)
+            indexR  = np.where(rG < R[i])
+            Menc[i] = np.sum(mG[indexR])
         
-        return Mass*1e10*u.Msun
+        return Menc*1e10*u.Msun
     
         """
         Create a caveat for M33
@@ -80,20 +78,14 @@ class MassProfile:
         Output: array pf total mass(B+D+H) at each radius of the input array
         """
     def MassEnclosedTotal(self, R):
-        #---find mass enclosed for each particle type
-        DiskMass = self.MassEnclosed(1,R)
-        HaloMass = self.MassEnclosed(2,R) 
+        #---find mass enclosed for each particle type)
+        Menc = self.MassEnclosed(1,R) + self.MassEnclosed(2,R) + self.MassEnclosed(3,R)
 
         #---for M33 since it contains no bulge
         if (self.gname == 'M33'):
-            BulgeMass = np.zeros(np.size(R))
-        else:
-            BulgeMass = self.MassEnclosed(3,R)
-            
-        BulgeMass= self.MassEnclosed(3,R)
-        masstot  = (DiskMass + HaloMass + BulgeMass)
-        
-        return masstot
+            Menc = self.MassEnclosed(1,R) + self.MassEnclosed(2,R)
+
+        return Menc
     
     """
     Compute the mass enclosed within a given radius
@@ -109,20 +101,15 @@ class MassProfile:
     Input:  ptype, array with radii
     Output: circular speed in units of km/s rounded
     """
-    def CircularVelocity(self, pytpe, R):
+    def CircularVelocity(self, ptype, R):
         #---constants
         Gcos = G.to(u.kpc*u.km**2/u.s**2/u.Msun)
-        #---like in mass enclosed follow the loop
+        Menc = self.MassEnclosed(ptype,R)
         #---rotaion curves V = (G*M(<R)/R)^0.5
-        #---loop over the radius
-        Vel = np.zeros(np.size(R))
-        num = 0
-        for i in range(np.size(R)):
-            v      = np.sqrt(Gcos*self.MassEnclosed(ptype,R)[num]/radii)
-            Vel[i] = v
-            num    = num+1
-            
-        return np.around(Vel,2)*u.km/u.s
+        Vcirc = np.round(np.sqrt(G*Menc/R/u.kpc),2)
+        
+        return Vcirc
+
 
     """
     Total CV is NOT just the CV of each individual galaxy component summed together
@@ -131,117 +118,181 @@ class MassProfile:
             at each radius of the input array
     """
     def CircularVelocityTotal(self, R):
-        return R
+        Menc = self.MassEnclosedTotal(R)
+        Vcirc = np.round(np.sqrt(G*Menc/R/u.kpc),2)
+
+        return  Vcirc 
 
     """
     Compute the circular speed using HR mass profile, can call it here
     Input:  radius, scale factor, halo mass
     Output: circular speed in units of km/s rounded
     """
-    def HernquistVCirc(self,R):
-        return R
+    def HernquistVCirc(self,R, a, Mhalo):
+        Menc = self.HernquistMass(R,a,Mhalo)
+        Vcirc = np.round(np.sqrt(G*Menc/R/u.kpc),2)
+        #---returns array Vcirc
+        
+        return Vcirc
 
 #---PLOTS
-
-#---plot using inclass lab as template
-figure = plt.figure(1)
-ax = plt.subplot(111)
+#---mass profiles
+MWP  = MassProfile('MW', 0)
+M31P = MassProfile('M31',0)
+M33P = MassProfile('M33',0)
 
 #---radius array of 30 kpc
 Rarry = np.arange(0.1, 30, 0.5)
 
-#---mass profiles
-MWP  = MassProfile('MW', 00)
-M31P = MassProfile('M31', 00)
-M33P = MassProfile('M33', 00)
 
+########################################################################################
+########################################################################################
+#---MW
+#---plot using inclass lab as template
+fig = plt.figure(figsize=(10,10))
+ax = plt.subplot(111)
 #---halo, disk, bulge, and total of MW
 MWH  = MWP.MassEnclosed(1,Rarry)
-M31H = M31P.MassEnclosed(1,Rarry)
-M33H = M33P.MassEnclosed(1,Rarry)
 MWD  = MWP.MassEnclosed(2,Rarry)
 MWB  = MWP.MassEnclosed(3,Rarry)
 MWT  = MWP.MassEnclosedTotal(Rarry)
 
-#print("MWH:", MWH)
-#print("MWD:", MWD)
-#print("MWB:", MWB)
-
 #---HR profile want to give total Halo Mass NOT massenc Galaxy Halo Mass
-#---had help from Drew about the a vaules, since I was running out of time
-MWHR  = MWP.HernquistMass(Rarry,  62, MWH)
-M31HR = M31P.HernquistMass(Rarry, 62, M31H)
-M33HR = M33P.HernquistMass(Rarry, 25, M33H)
+#---using vaules from previous assignments
+MWHR  = MWP.HernquistMass(Rarry,  62, 1.975e12)
 
-#---plotting halo, disk, bulge, total, and HR 
+#---plotting halo, disk, bulge, total, and HR for MW 
 plt.semilogy(Rarry, MWH,  color='blue',  label='Halo Mass')
 plt.semilogy(Rarry, MWD,  color='red',   label='Disk Mass')
 plt.semilogy(Rarry, MWB,  color='green', label='Bulge Mass')
 plt.semilogy(Rarry, MWT,  color='yellow',label='Total Mass')
-
 plt.semilogy(Rarry, MWHR, color='black', linestyle="--",label='Hernquist for MW a=62')
-plt.semilogy(Rarry, M31HR, color='black', linestyle=":",label='Hernquist for M31 a=62')
-plt.semilogy(Rarry, M33HR, color='black', linestyle="-",label='Hernquist for M33 a=25')
 
 #---titles~ things~
-plt.title('Milky Way Mass Profile')
 plt.xlabel('From Center of Mass [kpc]')
 plt.ylabel('Mass Enclosed (Msun)')
+plt.ylim(1e9,1e12)
 legend = ax.legend(loc='lower right')
+plt.figtext(0.15, 0.83, 'Milky Way Mass Profile', fontsize=22)
+########################################################################################
+#---M31
+fig = plt.figure(figsize=(10,10))
+ax = plt.subplot(111)
 
+M31H = M31P.MassEnclosed(1,Rarry)
+M31D = M31P.MassEnclosed(2,Rarry)
+M31B = M31P.MassEnclosed(3,Rarry)
+M31T = M31P.MassEnclosedTotal(Rarry)
 
-#############rotation curve################
-#---plotting rotaion curve
-figure = plt.figure(2)
-a2x = plt.subplot(111)
+M31HR = M31P.HernquistMass(Rarry, 62, 1.921e12)
+
+plt.semilogy(Rarry, M31H,  color='blue',  label='Halo Mass')
+plt.semilogy(Rarry, M31D,  color='red',   label='Disk Mass')
+plt.semilogy(Rarry, M31B,  color='green', label='Bulge Mass')
+plt.semilogy(Rarry, M31T,  color='yellow',label='Total Mass')
+plt.semilogy(Rarry, M31HR, color='black', linestyle="--",label='Hernquist for M31 a=62')
+
+plt.xlabel('From Center of Mass [kpc]')
+plt.ylabel('Mass Enclosed (Msun)')
+plt.ylim(1e9,1e12)
+legend = ax.legend(loc='lower right')
+plt.figtext(0.15, 0.83, 'M31 Mass Profile', fontsize=22)
+########################################################################################
+#---M33
+fig = plt.figure(figsize=(10,10))
+ax = plt.subplot(111)
+
+M33H = M33P.MassEnclosed(1,Rarry)
+M33D = M33P.MassEnclosed(2,Rarry)
+M33T = M33P.MassEnclosedTotal(Rarry)
+
+M33HR = M33P.HernquistMass(Rarry, 25, 0.187e12)
+
+plt.semilogy(Rarry, M33H,  color='blue',  label='Halo Mass')
+plt.semilogy(Rarry, M33D,  color='red',   label='Disk Mass')
+plt.semilogy(Rarry, M33T,  color='yellow',label='Total Mass')
+plt.semilogy(Rarry, M33HR, color='black', linestyle="--",label='Hernquist for M33 a=25')
+
+plt.xlabel('From Center of Mass [kpc]')
+plt.ylabel('Mass Enclosed (Msun)')
+plt.ylim(1e9,1e12)
+legend = ax.legend(loc='lower right')
+plt.figtext(0.15, 0.83, 'M33 Mass Profile', fontsize=22)
+########################################################################################
+########################################################################################
+#---rotaion curve MW
+fig = plt.figure(figsize=(10,10))
+ax = plt.subplot(111)
 
 #---assigning vel for halo mass (disk =2 and bulge =3) and total vel
-MWROT   = MWP.CircularVelocity(1,Rarry)
-M31ROT  = M31P.CircularVelocity(1,Rarry)
-M33ROT  = M33P.CircularVelocity(1,Rarry)
+MWHROT = MWP.CircularVelocity(1,Rarry)
+MWDROT = MWP.CircularVelocity(2,Rarry)
+MWBROT = MWP.CircularVelocity(3,Rarry)
+MWROTT = MWP.CircularVelocityTotal(Rarry)
 
-MWROTT  = MWP.CircularVelocityTotal(Rarry)
+#---hernquist Vcirc Profile
+MWHRCV = MWP.HernquistVCirc(Rarry, 62, 1.975e12)
+
+#---plot halo, disk, bulge, total, CVHR and totCV
+plt.plot(Rarry, MWHROT, color='purple', linestyle="--", label='Halo')
+plt.plot(Rarry, MWDROT, color='purple', linestyle=":", label='Disk')
+plt.plot(Rarry, MWBROT, color='purple', linestyle="-", label='Bulge')
+
+plt.plot(Rarry, MWROTT, color='blue', linestyle="--", label='Total')
+plt.plot(Rarry, MWHRCV, color='blue', linestyle="-", label='Hernquist')
+
+plt.xlabel('Radius (kpc)', fontsize=22)
+plt.ylabel('Circular Velocity (km/s)', fontsize=22)
+#plt.ylim(0,3)
+legend = ax.legend(loc='upper right')
+plt.figtext(0.15, 0.83, 'Milky Way Circular Velocity', fontsize=22)
+########################################################################################
+#---rotation curve M31
+fig = plt.figure(figsize=(10,10))
+ax = plt.subplot(111)
+
+M31HROT = M31P.CircularVelocity(1,Rarry)
+M31DROT = M31P.CircularVelocity(2,Rarry)
+M31BROT = M31P.CircularVelocity(3,Rarry)
 M31ROTT = M31P.CircularVelocityTotal(Rarry)
-M33ROTT = M33P.CircularVelocityTotal(Rarry)
 
-MWHRCV  = MWP.HernquistVCirc(Rarry,  62, MWROT)
-M31HRCV = M31P.HernquistVCirc(Rarry, 62, M31ROT)
-M33HRCV = M33P.HernquistVCirc(Rarry, 25, M33ROT)
+M31HRCV = M31P.HernquistVCirc(Rarry, 62, 1.921e12)
 
-#---can repeat this for disk mass and bulge mass
-plt.semilogy(Rarry, MWROT, color='purple', linestyle="--", label='Halo Mass for MW')
-plt.semilogy(Rarry, M31ROT, color='purple', linestyle=":", label='Halo Mass for M31')
-plt.semilogy(Rarry, M33ROT, color='purple', linestyle="-", label='Halo Mass for M33')
+plt.plot(Rarry, M31HROT, color='purple', linestyle="--", label='Halo')
+plt.plot(Rarry, M31DROT, color='purple', linestyle=":", label='Disk')
+plt.plot(Rarry, M31BROT, color='purple', linestyle="-", label='Bulge')
 
-#---total CV
-plt.semilogy(Rarry, MWROTT, color='pink', linestyle="-", label='Total Cricular Vel MW')
-plt.semilogy(Rarry, M31ROTT, color='pink', linestyle=":", label='Total Cricular Vel M31')
-plt.semilogy(Rarry, M33ROTT, color='pink', linestyle="-", label='Total Cricular Vel M33')
+plt.plot(Rarry, M31ROTT, color='blue', linestyle="--", label='Total')
+plt.plot(Rarry, M31HRCV, color='blue', linestyle="-", label='Hernquist')
 
-#---HR cric
-plt.semilogy(Rarry, MWHRCV, color='brown', linestyle="-", label=' HernquistVCirc MW')
-plt.semilogy(Rarry, M31HRCV, color='brown', linestyle=":", label='HernquistVCirc M31')
-plt.semilogy(Rarry, M33HRCV, color='brown', linestyle="-", label='HernquistVCirc M33')
+plt.xlabel('Radius (kpc)', fontsize=22)
+plt.ylabel('Circular Velocity (km/s)', fontsize=22)
+legend = ax.legend(loc='upper right')
+plt.figtext(0.15, 0.83, 'M31 Circular Velocity', fontsize=22)
+########################################################################################
+#---rotation curve M33
+fig = plt.figure(figsize=(10,10))
+ax = plt.subplot(111)
 
-#---titles~ things~
-plt.title('Milky Way Rotation Curve')
-plt.xlabel('From Center of Mass [kpc]')
-plt.ylabel('Mass Enclosed (Msun)')
-legend2 = a2x.legend(loc='lower right')
+M33HROT = M31P.CircularVelocity(1,Rarry)
+M33DROT = M31P.CircularVelocity(2,Rarry)
+M33ROTT = M31P.CircularVelocityTotal(Rarry)
+
+M33HRCV = M31P.HernquistVCirc(Rarry, 25, 0.187e12)
+
+plt.plot(Rarry, M33HROT, color='purple', linestyle="--", label='Halo')
+plt.plot(Rarry, M33DROT, color='purple', linestyle=":", label='Disk')
+
+plt.plot(Rarry, M33ROTT, color='blue', linestyle="--", label='Total')
+plt.plot(Rarry, M33HRCV, color='blue', linestyle="-", label='Hernquist')
+
+plt.xlabel('Radius (kpc)', fontsize=22)
+plt.ylabel('Circular Velocity (km/s)', fontsize=22)
+legend = ax.legend(loc='upper right')
+plt.figtext(0.15, 0.83, 'M33 Circular Velocity', fontsize=22)
+
 plt.show()
-
-#---notes
-"""
-I get an error from plotting the rotation curve
- File "RotationCurve.py", line 121, in CircularVelocity
-    v= np.sqrt(Gcos*self.MassEnclosed(ptype,R)[num]/radii)
-    NameError: global name 'ptype' is not defined
-"""
-
-
-
-        
-        
+   
         
 
 
