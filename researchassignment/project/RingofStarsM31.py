08a08a06d6792b03f42605d317ba680baea15940
fridajau <fjauregui@email.arizona.edu>
2018-05-03 19:42:04 -0700

    updated code

--- researchassignment/project/RingofStarsM31.py
+++ researchassignment/project/RingofStarsM31.py
@@ -7,142 +7,172 @@ from ReadFile import Read
 from CenterofMass import CenterOfMass
 
 #---class
-#---index disk particles from data to work only with disk particles
+#Create class that will take a galaxy, snapshot, and particle type
 class SolarParticles:
     def __init__(self, galaxy, snap, ptype):
+        #--add string to the text file format "000"
+        #--remove all but the last 3 digits
         ilbl = '000' + str(snap)
-        # remove all but the last 3 digits
         ilbl = ilbl[-3:]
-        # create filenames
+        #--create filenames
         self.filename='%s_'%(galaxy) + ilbl + '.txt'
-        self.time, self.total, self.data = Read('VLowRes/'+self.filename)
-        #--storing data for disk particles
-        self.index = np.where(self.data['type'] == ptype)
+        #--use HighRes
+        #--creating a COM for M31 using Disk Particles from CenterOfMass
+        self.COM_M31 = CenterOfMass('HighRes/'+self.filename,2)
+        M31_pos = self.COM_M31.COM_P(1.0,4.0)
+        M31_vel = self.COM_M31.COM_V(M31_pos[0],M31_pos[1],M31_pos[2])
         
-        self.m = self.data['m'][self.index]
-        self.x = self.data['x'][self.index]
-        self.y = self.data['y'][self.index]
-        self.z = self.data['z'][self.index]
-        self.vx = self.data['vx'][self.index]
-        self.vy = self.data['vy'][self.index]
-        self.vz = self.data['vz'][self.index]
-
-#--Index data to return vaules of M31 within a radial distance of 7-9 kpc.
-#--Contain  particles that are within half the scale hieght of M31
-
-    def RadialIndex(self, x,y,z):
-        #INPUT:  x,y,z postions
-        #OUTPUT: the ring of particles
-
-        #--use CenterOfMass to obtain the COM of x,y,z vx,xy,vz of M31
-        #--snap shots to look out for (0.0, 3.87, 5.87, 6.2, 10.0)
-        #--obtain the index ring of particles to use later on to define at wanted snap shots
-        #--follow this function for radial positions
-
-        #--creating a COM obeject for M31 using Disk Particles from CenterOfMass
-        COM_M31 = CenterOfMass('VLowRes/'+self.filename,2)
-        M31_pos = COM_M31.COM_P(1.0,4.0)
-        M31_vel = COM_M31.COM_V(M31_pos[0],M31_pos[1],M31_pos[2])
+        self.COMX = self.COM_M31.x - float(M31_pos[0]/u.kpc)
+        self.COMY = self.COM_M31.y - float(M31_pos[1]/u.kpc)
+        self.COMZ = self.COM_M31.z - float(M31_pos[2]/u.kpc)
+        
+        self.COMVX = self.COM_M31.vx - float(M31_vel[0]/(u.km/u.s))
+        self.COMVY = self.COM_M31.vy - float(M31_vel[1]/(u.km/u.s))
+        self.COMVZ = self.COM_M31.vz - float(M31_vel[2]/(u.km/u.s))
+        #--COM object at snapshot 0
+        COM_M310 = CenterOfMass('HighRes/M31_000.txt',2)
+        M31_pos0 = COM_M310.COM_P(1.0,4.0)
+        M31_vel0 = COM_M310.COM_V(M31_pos0[0],M31_pos0[1],M31_pos0[2])
         #--x,y,z pos & vel of m31 rel to COM
-        COMX = COM_M31.x - float(M31_pos[0]/u.kpc)
-        COMY = COM_M31.y - float(M31_pos[1]/u.kpc)
-        COMZ = COM_M31.z - float(M31_pos[2]/u.kpc)
+        COMX0 = COM_M310.x - float(M31_pos0[0]/u.kpc)
+        COMY0 = COM_M310.y - float(M31_pos0[1]/u.kpc)
+        COMZ0 = COM_M310.z - float(M31_pos0[2]/u.kpc)
+        #--radial distance from center of galaxy
+        RadPos = np.sqrt(COMX0**2 + COMY0**2)
+        #--index the vaules within 7-9 kpc CHANGE ZCOM
+        self.Rindex = np.where((RadPos > 7) & (RadPos < 9) & (COMZ0 > -5) & (COMZ0 < 5))
+
+
+
         
-        COMVX = COM_M31.vx - float(M31_vel[0]/(u.km/u.s))
-        COMVY = COM_M31.vy - float(M31_vel[1]/(u.km/u.s))
-        COMVZ = COM_M31.vz - float(M31_vel[2]/(u.km/u.s))
-        #--3d radial position from galatic center
-        RadPos = np.sqrt(COMX**2 + COMY**2 + COMZ**2)
-        #--index the vaules within 7-9 kpc 
-        Rindex = np.where((RadPos > 7) & (RadPos < 9) & (COMZ > -18.5) & (COMZ < 18.5))
-    
-        return Rindex
-    
-    def RadialPos(self, x,y,z):
-        #INPUT: x,y,z positions, center of galaxy=0  
-        #RETURNS: 3d coord of pcom or vcom of particals within 7-9 kpc a snap shot 0
-
-        #--index M31 vaules and return an array with the desired particles
-
-        COM_M31 = CenterOfMass('VLowRes/'+self.filename,2)
-        M31_pos = COM_M31.COM_P(1.0,4.0)
-        M31_vel = COM_M31.COM_V(M31_pos[0],M31_pos[1],M31_pos[2])
-       
-        COMX = COM_M31.x - float(M31_pos[0]/u.kpc)
-        COMY = COM_M31.y - float(M31_pos[1]/u.kpc)
-        COMZ = COM_M31.z - float(M31_pos[2]/u.kpc)
+        #--functions
+    #Obtain radial positions not at com position for any galaxy
+    def RadialPos(self):
+        #INPUT:   self
+        #RETURNS: pos with the radial index
         
-        COMVX = COM_M31.vx - float(M31_vel[0]/(u.km/u.s))
-        COMVY = COM_M31.vy - float(M31_vel[1]/(u.km/u.s))
-        COMVZ = COM_M31.vz - float(M31_vel[2]/(u.km/u.s))
+        return self.COM_M31.x[self.Rindex], self.COM_M31.y[self.Rindex], self.COM_M31.z[self.Rindex]
 
-        #--3d radial position from galatic center
-        RadPos = np.sqrt(COMX**2 + COMY**2 + COMZ**2)
-        #--use Rindex function to always return the ring of particles
-        Rindex = self.RadialIndex(COMX,COMY,COMZ)
-        nM31x = COMX[Rindex]
-        nM31y = COMY[Rindex]
-        nM31z = COMZ[Rindex]
+    
+    #Obtain radial positions AT com pos & vel(if needed)
+    def RadialPosCOM(self):
+        #INPUT:   self  
+        #RETURNS: 3d coord of pcom or vcom of particals within 7-9 kpc
 
-        nM31vx = COMVX[Rindex]
-        nM31vy = COMVY[Rindex]
-        nM31vz = COMVZ[Rindex]
+        #--use the Rindex to always return the ring of particles at COM of x,y,z,vx,vy,vz
+        nM31x = self.COMX[self.Rindex]
+        nM31y = self.COMY[self.Rindex]
+        nM31z = self.COMZ[self.Rindex]
+
+        nM31vx = self.COMVX[self.Rindex]
+        nM31vy = self.COMVY[self.Rindex]
+        nM31vz = self.COMVZ[self.Rindex]
 
         return nM31x, nM31y, nM31z, nM31vx, nM31vy, nM31vz
 
-#--testing code
-#--m31
-Disk_M31    = SolarParticles("M31", 700, 2)
-radial_ring = Disk_M31.RadialPos(-377,608,-284)
-testx  = np.around(radial_ring[0],3)
-testy  = np.around(radial_ring[1],3)
-testz  = np.around(radial_ring[2],3)
-testvx = np.around(radial_ring[3],3)
-testvy = np.around(radial_ring[4],3)
-testvz = np.around(radial_ring[5],3)
-#--mw
-Disk_MW    = CenterOfMass("MW_700.txt", 2)
-MW_pos     = Disk_MW.COM_P(1.0,4.0)
-xmw = Disk_MW.x - float(MW_pos[0]/u.kpc)
-ymw = Disk_MW.y - float(MW_pos[1]/u.kpc)
-zmw = Disk_MW.z - float(MW_pos[2]/u.kpc)
-#--m33
-Disk_M33 = CenterOfMass("M33_700.txt", 2)
-M33_pos  = Disk_M33.COM_P(1.0,4.0)
-xm33 = Disk_M33.x - float(M33_pos[0]/u.kpc)
-ym33 = Disk_M33.y - float(M33_pos[1]/u.kpc)
-zm33 = Disk_M33.z - float(M33_pos[2]/u.kpc)
+    #def IntoM33(self):
+    
 
-#--plot testing
+    
+#Obtain positions of particles for M31, MW, M33
+#--m31
+Ring_M31    = SolarParticles("M31", 700, 2)  #--"galaxy", snapshot, disk particles
+radial_ring = Ring_M31.RadialPosCOM()        #--RadialCOM() centering M31
+xm31  = np.around(radial_ring[0],3)
+ym31  = np.around(radial_ring[1],3)
+zm31  = np.around(radial_ring[2],3)
+
+#--m31 all
+Disk_M31= CenterOfMass("M31_000.txt", 2)
+M31_pos = Disk_M31.COM_P(1.0,4.0)
+Dxm31 = Disk_M31.x #- float(MW_pos[0]/u.kpc)   #--commented out COM for real locations at given snapshot 
+Dym31 = Disk_M31.y #- float(MW_pos[1]/u.kpc)    
+Dzm31 = Disk_M31.z #- float(MW_pos[2]/u.kpc)
+
+#--mw
+Disk_MW = CenterOfMass("MW_000.txt", 2)
+MW_pos  = Disk_MW.COM_P(1.0,4.0)
+xmw = Disk_MW.x #- float(MW_pos[0]/u.kpc)
+ymw = Disk_MW.y #- float(MW_pos[1]/u.kpc)
+zmw = Disk_MW.z #- float(MW_pos[2]/u.kpc)
+
+#--m33
+Disk_M33 = CenterOfMass("M33_000.txt", 2)
+M33_pos  = Disk_M33.COM_P(1.0,4.0)
+xm33 = Disk_M33.x #- float(M33_pos[0]/u.kpc)
+ym33 = Disk_M33.y #- float(M33_pos[1]/u.kpc)
+zm33 = Disk_M33.z #- float(M33_pos[2]/u.kpc)
+
+
+#--plots
+#Make a histogram for M31 vs MW (look at M33)
 fig = plt.figure(figsize=(10,10))
 ax = plt.subplot(111)
-plt.hist2d(testx, testy, bins=232, norm=LogNorm(), cmap='magma')
+
+#--hist for M31
+plt.hist2d(xm31, ym31, bins=600, norm=LogNorm(), cmap='magma')
 plt.colorbar()
-#plt.hist2d(xm33, ym33, bins=840, norm=LogNorm(), cmap='Greens')
+#--hist for MW
+#plt.hist2d(xmw, ymw, bins=650, norm=LogNorm(), cmap='Reds')
 #plt.colorbar()
-#plt.hist2d(xmw, ymw, bins=850, norm=LogNorm(), cmap='Reds')
+#--hist for M33
+#plt.hist2d(xm33, ym33, bins=650, norm=LogNorm(), cmap='Blues')
 #plt.colorbar()
-plt.text(-38, 38,'T=10.0')
+#--label for snap shot
+#plt.text(-50, 60,'T=3.87')
 plt.xlabel('x (kpc)', fontsize=22)
 plt.ylabel('y (kpc)', fontsize=22)
 label_size = 22
 matplotlib.rcParams['xtick.labelsize'] = label_size 
 matplotlib.rcParams['ytick.labelsize'] = label_size
-plt.ylim(-40,40)
-plt.xlim(-40,40)
+#plt.ylim(-150,150)
+#plt.xlim(-130,130)
 plt.show()
 
+#########################################################################################
 
+####Ratio of Sun Candidates at a Given Snapshot####
 
-#--ratio of sun candinates
-ratioring = np.sqrt(testx**2 + testy**2 + testz**2)
+#--selected particles at a choosen snapshot, from above initialization(700), at COM
+ringCOM   =  Ring_M31.RadialPosCOM()
+ring_part = np.sqrt(ringCOM[0]**2 + ringCOM[1]**2 + ringCOM[2]**2)
+#--all ring particles
+ring_tot  = np.size(ringCOM[0])
+print("magnitude of ring of part",ring_tot)
 
-#--plots for ratio of sun candinates
+#--initialize a new object at snap0 COM
+diskm31_S = SolarParticles("M31", 000, 2)
+ringcom_S    = diskm31_S.RadialPosCOM()
+ringofpart_S = np.sqrt(ringcom_S[0]**2 + ringcom_S[1]**2 + ringcom_S[2]**2)
+
+#--plots
+#Make a histogram plot thatll show the radial distribution of candidate suns w/
+#respect to the center of the MW & M31 remnant
 fig = plt.figure(figsize=(10,10))
 ax = plt.subplot(111)
 plt.xlabel('Distance rom Merger Remnant (kpc)', fontsize=22)
 plt.ylabel('Solar Candinates', fontsize=22)
-plt.hist(ratioring, bins=50, color='black', histtype="step")
+
+#--hist of ring of particles
+plt.hist(ring_part, bins=120, color='black', histtype="step")
+#--hist of disk particles at snapshot0 
+plt.hist(ringofpart_S, bins=120, color='blue', histtype="step")
+#--add lines to label plot
 plt.axvline(8.29, color='red', ls='--')
-plt.axis([0, 160, 0, 0.5])
+plt.text(8.29,250,'Current Distance [8.29 kpc]',rotation=90)
+plt.text(40,90,'Black: T=10.0')
+plt.text(40,80,'Blue:  T=0.0')
+#plt.xticks([20,40,60,80,100])
+#plt.axis([0, 150, 0, 400])
 plt.show()
+
+"""
+new function
+use center of mass to find half the radius of m33
+find m33 com from center of mass
+use radpos to get the x,y,z positions of m33
+rindex for range of radii
+
+delta_r must be less than r/2 of m33
+returns some seperation of x - m33com.x
+"""
